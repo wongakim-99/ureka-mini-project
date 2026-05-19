5161f77419491bdf69571a8a0653109f9e2ca0ae
Lee Hosung <leehosung503@naver.com>
2026-05-19 14:30:05 +0900

    잔여 좌석 확인 로직 추가

--- src/main/java/domain/reservation/ReservationService.java
+++ src/main/java/domain/reservation/ReservationService.java
@@ -1,19 +1,26 @@
 package domain.reservation;
 
-import common.ComboItem;
 import java.sql.SQLException;
 import java.util.List;
+
+import common.ComboItem;
 
 public class ReservationService {
 
 	private final ReservationRepository repository;
+	private static final String SOLD_OUT_MESSAGE = "남은 좌석이 없습니다. 예약할 수 없습니다.";
 
 	public ReservationService(ReservationRepository repository) {
 		this.repository = repository;
 	}
 
 	public List<Reservation> findAll() throws SQLException             { return repository.findAll(); }
-	public void save(Reservation reservation) throws SQLException      { repository.save(reservation); }
+	public void save(Reservation reservation) throws SQLException {
+		if (repository.findRemainSeatsByScreening(reservation.getScreenId()) <= 0) {
+			throw new SQLException(SOLD_OUT_MESSAGE);
+		}
+		repository.save(reservation);
+	}
 	public void update(Reservation reservation) throws SQLException    { repository.update(reservation); }
 	public void delete(int reservId) throws SQLException               { repository.delete(reservId); }
 	public List<ComboItem> getCustomerOptions() throws SQLException              { return repository.findCustomerOptions(); }
